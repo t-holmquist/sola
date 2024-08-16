211acf9ab627034a0a16d76b44e976439fc0a58a
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-08-16 10:45:04 +0200

    lowered image sizing further

--- components/kaffebarGallery.tsx
+++ components/kaffebarGallery.tsx
@@ -5,29 +5,29 @@ const KaffebarGallery = () => {
     <div className="grid grid-cols-12 gap-8 sm:gap-12 px-5 sm:px-32 sm:mt-[140px] mb-[120px] sm:mb-[200px]">
         <Image
         src='/kaffebar2.webp'
-        width={960}
-        height={540}
+        width={660}
+        height={240}
         alt="community"
         className="rounded-2xl col-span-12 md:col-span-6"
         />
         <Image
         src='/kaffebar1.webp'
-        width={960}
-        height={540}
+        width={660}
+        height={240}
         alt="community"
         className="rounded-2xl col-span-12 md:col-span-6"
         />
         <Image
         src='/kaffebar4.webp'
-        width={960}
-        height={540}
+        width={660}
+        height={240}
         alt="community"
         className="rounded-2xl col-span-12 md:col-span-6"
         />
         <Image
         src='/kaffebar3.webp'
-        width={960}
-        height={540}
+        width={660}
+        height={240}
         alt="community"
         className="rounded-2xl col-span-12 md:col-span-6"
         />
