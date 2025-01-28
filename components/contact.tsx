d23eb2b1552b92b10018f82c62a5d08cd52d2079
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-01-28 19:42:32 +0100

    update Contact component layout and image positioning

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,9 +6,9 @@ export const Contact = () => {
   return (
     <div className="flex relative flex-col items-center mt-28 lg:mt-0 gap-8">
       <div className="flex flex-col gap-8">
-        <Image src='/line-drawing.png' width={400} height={250} alt="people-line-drawing" className="absolute hidden lg:block -top-20 -left-0"/>
-        <Image src='/line-drawing.png' width={200} height={250} alt="people-line-drawing" className="absolute -top-[150px] lg:hidden"/>
-        <div className="flex flex-col gap-3 p-4 border-accent border bg-backgroundSandDark rounded-2xl ">
+        <Image src='/line-drawing.png' width={450} height={250} alt="people-line-drawing" className="absolute hidden lg:block -top-20 left-10 z-10"/>
+        <Image src='/line-drawing.png' width={250} height={250} alt="people-line-drawing" className="absolute -top-[150px] lg:hidden z-10"/>
+        <div className="flex flex-col gap-3 p-4 border-accent border bg-backgroundSandDark rounded-2xl relative z-20">
           <h2 className="text-5xl md:text-6xl font-gothic text-black">Pris på kontorplads</h2>
           <p className="font-sans font-bold text-accent">2200 kr. ex moms</p>
           <p className="text-black font-sans sm:max-w-lg max-w-80">Kom forbi og få en kop kaffe, eller send os en mail og hør mere om vores faciliteter, området eller vores fremtidige planer for kontorfællesskabet.</p>
--- components/imageChanger.tsx
+++ components/imageChanger.tsx
@@ -17,7 +17,7 @@ export const ImageChanger = () => {
         key={currentImage}
         className="flex flex-col gap-8 items-center">
             <div className="flex gap-4">
-                <button onClick={() => {setCurrentImage("/øvreplan.webp")}} className={`${currentImage == "/øvreplan.webp" ? "bg-accent text-white" : "border border-accent text-black"}  px-4 py-2 cursor-default`}>Øvre plan</button>
+                {/* <button onClick={() => {setCurrentImage("/øvreplan.webp")}} className={`${currentImage == "/øvreplan.webp" ? "bg-accent text-white" : "border border-accent text-black"}  px-4 py-2 cursor-default`}>Øvre plan</button> */}
                 {/* <button onClick={() => {setCurrentImage("/menu-drikke.webp")}} className={`${currentImage == "/menu-drikke.webp" ? "bg-accent text-white" : "border border-accent text-black"}  px-4 py-2 rounded-xl sm:hover:scale-105 sm:transition-all`}>Nedre plan</button> */}
             </div>
             <motion.div
