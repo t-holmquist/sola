05841e052ff37ca1a08133ccd0f21468de3c77d2
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-02-03 12:04:41 +0100

    refactor: large screen image responsiveness

--- components/floorplan.tsx
+++ components/floorplan.tsx
@@ -8,13 +8,13 @@ export const Floorplan = () => {
             <div className="flex flex-col lg:col-span-6 gap-10">
                 <Image src={"/illustrations/solakontor.webp"} width={550} height={550} alt="SOLA" className="aspect-video object-cover"/>
                 <h2 className="font-gothic font-bold text-5xl md:text-6xl">Sådan kan dit <span className="text-accent">kommende kontor se ud</span></h2>
-                <p className="font-sans font-medium">Kontorfælleskabet er fordelt på to planer. Vi har 10 pladser i alt og har alle faciliteter, der forventes af et anstændigt kontor.</p>
+                <p className="font-sans font-medium max-w-3xl">Kontorfælleskabet er fordelt på to planer. Vi har 10 pladser i alt og har alle faciliteter, der forventes af et anstændigt kontor.</p>
                 <div className="flex gap-2">
                     <div className="mt-1.5">
                         <Image src={"/icons/building-solid.svg"} width={35} height={35} alt="SOLA"/>
                     </div>
                     <div>
-                        <p className="font-sans font-medium"><span className="font-bold">Rummeligt kontor med mødelokale. </span>Kontoret er ca. 50 kvm, udrustet med hurtigt internet (1000/1000) og har mødelokale med fladskærm og whiteboard.</p>
+                        <p className="font-sans font-medium max-w-3xl"><span className="font-bold">Rummeligt kontor med mødelokale. </span>Kontoret er ca. 50 kvm, udrustet med hurtigt internet (1000/1000) og har mødelokale med fladskærm og whiteboard.</p>
                     </div>
                 </div>
                 <div className="flex gap-2 sm:gap-1">
@@ -22,7 +22,7 @@ export const Floorplan = () => {
                         <Image src={"/icons/clock-solid.svg"} width={30} height={30} alt="SOLA"/>
                     </div>
                     <div>
-                        <p className="font-sans font-medium"><span className="font-bold">God beliggenhed og åbent 24/7. </span>Vi ligger blot et stenkast fra motorvejsafkørsel, s-tog, bus og hyggelige grønne områder.</p>
+                        <p className="font-sans font-medium max-w-3xl"><span className="font-bold">God beliggenhed og åbent 24/7. </span>Vi ligger blot et stenkast fra motorvejsafkørsel, s-tog, bus og hyggelige grønne områder.</p>
                     </div>
                 </div>
             </div>
--- components/officeGallery.tsx
+++ components/officeGallery.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export const OfficeGallery = () => {
     return (
-        <section className="grid grid-cols-1 lg:grid-cols-12 gap-10 mt-20">
-            <div className="col-span-6 border">
-                <Image src={"/kontor2.webp"} width={800} height={600} alt="kontor" className="rounded-2xl border border-accent aspect-video object-cover" />
+        <section className="grid grid-cols-1 lg:grid-cols-12 gap-10 mt-20 items-end">
+            <div className="col-span-6">
+                <Image src={"/kontor2.webp"} width={800} height={600} alt="kontor" className="rounded-2xl border border-accent aspect-video object-cover w-full" />
             </div>
             <div className="col-span-6">
-                <Image src={"/kontor3_16x9.webp"} width={960} height={540} alt="kontor" className="rounded-2xl border border-accent aspect-video object-cover" />
+                <Image src={"/kontor3_16x9.webp"} width={960} height={540} alt="kontor" className="rounded-2xl border border-accent aspect-video object-cover w-full" />
             </div>
         </section>
     );
