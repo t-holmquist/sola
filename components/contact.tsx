e4720b9ee5a8487fedc3f7bcf32bf9bfd173050c
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-11-15 19:40:01 +0100

    update price

--- components/contact.tsx
+++ components/contact.tsx
@@ -10,7 +10,7 @@ export const Contact = () => {
         <Image src='/line-drawing.png' width={250} height={250} alt="people-line-drawing" className="absolute -top-[150px] lg:hidden z-10"/>
         <div className="flex flex-col gap-3 p-4 border-accent border bg-backgroundSandDark rounded-2xl relative z-20">
           <h2 className="text-5xl md:text-6xl font-gothic text-black">Pris på kontorplads</h2>
-          <p className="font-sans font-bold text-2xl text-accent">1000 kr/måneden resten af året</p>
+          <p className="font-sans font-bold text-2xl text-accent">{`1000 kr/måneden resten af året (normalpris 1950 kr)`}</p>
           <p className="text-black font-sans font-medium sm:max-w-lg max-w-80">Kom forbi og få en kop kaffe, eller send os en mail og hør mere om vores faciliteter, området eller vores fremtidige planer for kontorfællesskabet.</p>
         </div>
         <ContactForm />
